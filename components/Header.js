43ec00ead5f5dbe68740584f424b227efe064c5f
fbrcode <fabio.bressler@gmail.com>
2022-07-28 18:08:24 -0300

    minimalistic header

--- components/Header.js
+++ components/Header.js
@@ -1,5 +1,16 @@
 import { ConnectButton } from 'web3uikit';
+import Link from 'next/link';
 
 export default function Header() {
-  return <ConnectButton />;
+  return (
+    <nav>
+      <Link href="/">
+        <a>NFT Marketplace</a>
+      </Link>
+      <Link href="/sell-nft">
+        <a>Sell NFT</a>
+      </Link>
+      <ConnectButton />
+    </nav>
+  );
 }
