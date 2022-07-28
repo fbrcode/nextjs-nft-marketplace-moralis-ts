d09556b41c4ea46117dc4fb75650c9460976b278
fbrcode <fabio.bressler@gmail.com>
2022-07-28 18:23:26 -0300

    transfer head to app.js

--- pages/index.js
+++ pages/index.js
@@ -1,16 +1,7 @@
-import Head from 'next/head';
 import Image from 'next/image';
 import styles from '../styles/Home.module.css';
 
 export default function Home() {
-  return (
-    <div className={styles.container}>
-      <Head>
-        <title>NFT Marketplace</title>
-        <meta name="description" content="NFT Marketplace" />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-      Hi!
-    </div>
-  );
+  // show recent listed NFTs
+  return <div className={styles.container}>Hi!</div>;
 }
