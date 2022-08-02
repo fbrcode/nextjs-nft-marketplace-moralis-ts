7a17fba0e59df74621d5617b5a8fa4413ea2c290
fbrcode <fabio.bressler@gmail.com>
2022-08-02 07:40:50 -0300

    index nft component usage

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 import Image from 'next/image';
 import styles from '../styles/Home.module.css';
-import { useMoralisQuery } from 'react-moralis';
+import { useMoralisQuery, useMoralis } from 'react-moralis';
+import NFTBox from '../components/NFTBox';
 
 export default function Home() {
   // How do we show recent listed NFTs?
@@ -11,32 +12,51 @@ export default function Home() {
   // Moralis Server: NFT Marketplace
   // https://admin.moralis.io/dapps/details/198884
   // https://docs.moralis.io/moralis-dapp/automatic-transaction-sync/smart-contract-events
+  const { isWeb3Enabled } = useMoralis();
   const { data: listedNfts, isFetching: fetchingListedNfts } = useMoralisQuery(
     'ActiveItem', // TableName
     (query) => query.limit(10).descending('tokenId') // can also use .skip() for pagination
   );
-  console.log(listedNfts);
+
+  const myKey = 12345;
 
   return (
-    <div className={styles.container}>
-      {fetchingListedNfts ? (
-        <div>Loading...</div>
-      ) : (
-        listedNfts.map((nft) => {
-          console.log(nft.attributes);
-          const { marketplaceAddress, nftAddress, tokenId, price, seller } = nft.attributes;
-          // marketplaceAddress: "0xe7f1725e7734ce288f8367e1bb143e90bb3f0512"
-          // nftAddress: "0x5fbdb2315678afecb367f032d93f642f64180aa3"
-          // price: "100000000000000000"
-          // seller: "0xf39fd6e51aad88f6f4ce6ab8827279cfffb92266"
-          // tokenId: "1"
-          return (
-            <div>
-              Price: {price}. NftAddress: {nftAddress}. TokenId: {tokenId}. Seller: {seller}{' '}
-            </div>
-          );
-        })
-      )}
+    <div className="container mx-auto">
+      <h1 className="py-4 px-4 font-bold text-2xl">Recently Listed</h1>
+      <div className="flex flex-wrap">
+        {isWeb3Enabled ? (
+          fetchingListedNfts ? (
+            <div>Loading...</div>
+          ) : (
+            listedNfts.map((nft) => {
+              console.log(`NFT Attributes:`);
+              console.log(nft.attributes);
+              const { marketplaceAddress, nftAddress, tokenId, price, seller } = nft.attributes;
+              const key = nftAddress.toString() + tokenId.toString();
+              console.log(`Key: ${key}`);
+              // marketplaceAddress: "0xe7f1725e7734ce288f8367e1bb143e90bb3f0512"
+              // nftAddress: "0x5fbdb2315678afecb367f032d93f642f64180aa3"
+              // price: "100000000000000000"
+              // seller: "0xf39fd6e51aad88f6f4ce6ab8827279cfffb92266"
+              // tokenId: "1"
+              return (
+                <div>
+                  <NFTBox
+                    key={key}
+                    marketplaceAddress={marketplaceAddress}
+                    nftAddress={nftAddress}
+                    tokenId={tokenId}
+                    price={price}
+                    seller={seller}
+                  />
+                </div>
+              );
+            })
+          )
+        ) : (
+          <div>Web3 Currently Not Enabled</div>
+        )}
+      </div>
     </div>
   );
 }
