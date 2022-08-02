49d6b1dbf1547484500fa213f5c5fbee5c4ad095
fbrcode <fabio.bressler@gmail.com>
2022-08-02 17:27:37 -0300

    wrap up notifications component

--- pages/_app.js
+++ pages/_app.js
@@ -2,6 +2,7 @@ import '../styles/globals.css';
 import { MoralisProvider } from 'react-moralis';
 import Header from '../components/Header';
 import Head from 'next/head';
+import { NotificationProvider } from 'web3uikit';
 
 const APP_ID = process.env.NEXT_PUBLIC_MORALIS_APP_ID;
 const SERVER_URL = process.env.NEXT_PUBLIC_MORALIS_SERVER_URL;
@@ -16,8 +17,10 @@ function MyApp({ Component, pageProps }) {
       </Head>
       {/* connect the application to the Moralis server */}
       <MoralisProvider appId={APP_ID} serverUrl={SERVER_URL}>
-        <Header />
-        <Component {...pageProps} />
+        <NotificationProvider>
+          <Header />
+          <Component {...pageProps} />
+        </NotificationProvider>
       </MoralisProvider>
     </div>
   );
